c0e3d07282baec6fb67fe19ec1a4b324222c8e9e
OLEG USOV <kilogran18@gmail.com>
2022-01-27 21:00:47 +0300

    Первые попытки:  - Составлен манифест  - Добавлена jQuery  - Добавлен основной js-файл

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var SwaggerJSONViewer = 10000;
+
+console.log($(".highlight-code > .microlight"));
+
+setInterval(function(){
+    console.log('Onload...')
+    console.log($(".live-responses-table .highlight-code > .microlight"));
+}, 2000);
